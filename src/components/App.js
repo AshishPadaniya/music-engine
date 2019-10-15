ac3800f156d28219d387c724e9c8925a706927c3
AshishPadaniya <padaniyaaashish@gmail.com>
2019-10-15 12:13:09 -0400

    added button and textbox for search

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,28 @@ import React, {Component} from 'react';
 
 class App extends Component{
 
+   state = { artistQuery: ' '}
+
+   updateArtistQuery = event => {
+      console.log('event.target.value query' , event.target.value)
+      this.setState({ artistQuery: event.target.value})
+   }
+
+   searchArtist = () =>
+   {
+      console.log('this.state', this.state)
+   }
+   
+
  render(){
      return(
-        <div> React App</div>
+        <div> 
+           <h2> Music Engine </h2>
+           <input 
+               onChange={this.updateArtistQuery}  
+               placeholder='Search for music artist'></input>
+           <button onClick={this.searchArtist}>Search</button>
+        </div>
      )
    }
 }
